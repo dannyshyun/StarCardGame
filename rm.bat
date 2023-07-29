bbfb4812621874d6fb9655d020d56d237b03ca9b
shyun <c4521c@gmail.com>
2023-07-29 12:24:59 +0900

    rm update

--- rm.bat
+++ rm.bat
@@ -1,3 +1,4 @@
 FOR /d /r . %%d IN (".vs") DO @IF EXIST "%%d" rd /s /q "%%d"
 FOR /d /r . %%d IN ("x64") DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN (".build") DO @IF EXIST "%%d" rd /s /q "%%d"
 pause
